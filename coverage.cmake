02e9ddf94d554bef9e8abb6ad5fe4dcdf3c82bfe
Julian LALU <julian.lalu@outlooK.fr>
2024-12-30 15:15:25 +0100

    Test new enable_coverage

--- coverage.cmake
+++ coverage.cmake
@@ -209,6 +209,62 @@ elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
 				-o coverage.linux.clang.lcov.info
 				..
 	)
+elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+	# # Disable compiler batching to fix a clang-cl bug when activate --coverage
+	# # See: https://developercommunity.visualstudio.com/t/Clang-cl---coverage-option-create-gcno-w/10253777
+	# set_property(TARGET ${project_name} PROPERTY VS_NO_COMPILE_BATCHING ON)
+	# set_property(TARGET ${lib_name} PROPERTY VS_NO_COMPILE_BATCHING ON)
+
+	target_compile_options(${project_name} PRIVATE --coverage)
+	target_link_options(${project_name} PRIVATE --coverage)
+	target_compile_options(${lib_name} PRIVATE --coverage)
+	
+	add_custom_command( 
+		TARGET ${project_name} POST_BUILD
+		COMMAND echo Download Grcov...
+		COMMAND if [ ! -e grcov ];then (curl -L https://github.com/mozilla/grcov/releases/latest/download/grcov-x86_64-unknown-linux-gnu.tar.bz2 | tar jxf -) fi
+	)
+	
+	add_custom_command( 
+		TARGET ${project_name} POST_BUILD
+		COMMAND echo Start coverage...
+		COMMAND ./${project_name}
+	)
+
+	add_custom_command( 
+		TARGET ${project_name} POST_BUILD
+		COMMAND echo Generate HTML report...
+		COMMAND ./grcov -t html -b . -s ./../../
+				--llvm-path /usr/bin/
+				#--branch
+				--keep-only "src/*" 
+				--keep-only "interface/*"
+				--excl-start "^.*LCOV_EXCL_START.*" 
+				--excl-stop "^.*LCOV_EXCL_STOP.*" 
+				--excl-line "\"(\\s*^.*GTEST_TEST\\.*)|(^.*LCOV_EXCL_LINE.*)\"" 
+				--excl-br-start "^.*LCOV_EXCL_START.*" 
+				--excl-br-stop "^.*LCOV_EXCL_STOP.*" 
+				--excl-br-line "\"(\\s*^.*GTEST_TEST\\.*)|(^.*LCOV_EXCL_BR_LINE.*)\"" 
+				-o linux.gcc
+				..
+	)
+	add_custom_command( 
+		TARGET ${project_name} POST_BUILD
+		COMMAND echo Generate LCOV report...
+		COMMAND ./grcov -t lcov -b . -s ./../../
+				--llvm-path /usr/bin/
+				#--branch
+				--keep-only "src/*"
+				--keep-only "interface/*"
+				--excl-start "^.*LCOV_EXCL_START.*" 
+				--excl-stop "^.*LCOV_EXCL_STOP.*" 
+				--excl-line "\"(\\s*^.*GTEST_TEST\\.*)|(^.*LCOV_EXCL_LINE.*)\"" 
+				--excl-br-start "^.*LCOV_EXCL_START.*" 
+				--excl-br-stop "^.*LCOV_EXCL_STOP.*" 
+				--excl-br-line "\"(\\s*^.*GTEST_TEST\\.*)|(^.*LCOV_EXCL_BR_LINE.*)\"" 
+				-o coverage.linux.gcc.lcov.info
+				..
+	)
 endif()
 endfunction()
 
